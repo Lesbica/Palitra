48938be525ca7708aa1aa0ba26a6baec5be3cc8c
Prykhodchenko Oleksandra <119930416+Lesbica@users.noreply.github.com>
2023-11-03 15:00:16 +0200

    furniture

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('.nav-item').forEach(navItem => {
     navItem.addEventListener('mouseenter', () => {
         const select = navItem.querySelector('select');
-        select.style.display = 'none';
     });
 });
--- src/tree.js
+++ src/tree.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -10,18 +11,9 @@ document.getElementById("canvas-container").appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// Создание объектов
-const geometry1 = new THREE.BoxGeometry(1, 1, 1);
-const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube1 = new THREE.Mesh(geometry1, material1);
+const loader = new OBJLoader();
+let loadedModel = null;
 
-const geometry2 = new THREE.SphereGeometry(0.5, 32, 32);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const sphere1 = new THREE.Mesh(geometry2, material2);
-
-let currentObject = null;
-
-// Настройка камеры
 camera.position.set(0, 0, 5);
 controls.update();
 
@@ -34,27 +26,96 @@ const animate = function () {
 animate();
 
 // Обработчики событий для добавления объектов
-document.getElementById("add-room1").addEventListener("click", () => {
-    if (currentObject) {
-        scene.remove(currentObject);
+document.getElementById("chair").addEventListener("click", () => {
+    if(loadedModel){
+        scene.remove(loadedModel);
     }
-    currentObject = cube1.clone();
-    scene.add(currentObject);
-    controls.target.set(currentObject.position.x, currentObject.position.y, currentObject.position.z);
-    controls.update();
+    loader.load(
+        // resource URL
+        'static/Chair.obj',
+        // called when resource is loaded
+        function ( object ) {
+            loadedModel = object;
+            scene.add( object );
+    
+        },
+        // called when loading is in progresses
+        function ( xhr ) {
+    
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    
+        },
+        // called when loading has errors
+        function ( error ) {
+    
+            console.log( 'An error happened' );
+    
+        }
+    );
 });
 
-document.getElementById("add-room2").addEventListener("click", () => {
-    if (currentObject) {
-        scene.remove(currentObject);
+document.getElementById("sofa").addEventListener("click", () => {
+    if(loadedModel){
+        scene.remove(loadedModel);
     }
-    currentObject = sphere1.clone();
-    scene.add(currentObject);
-    controls.target.set(currentObject.position.x, currentObject.position.y, currentObject.position.z);
-    controls.update();
+    loader.load(
+        // resource URL
+        'static/Koltuk.obj',
+        // called when resource is loaded
+        function ( object ) {
+            loadedModel = object;
+            scene.add( object );
+    
+        },
+        // called when loading is in progresses
+        function ( xhr ) {
+    
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    
+        },
+        // called when loading has errors
+        function ( error ) {
+    
+            console.log( 'An error happened' );
+    
+        }
+    );
+});
+
+document.getElementById("bed").addEventListener("click", () => {
+    if(loadedModel){
+        scene.remove(loadedModel);
+    }
+    loader.load(
+        // resource URL
+        'static/Bed.obj',
+        // called when resource is loaded
+        function ( object ) {
+            loadedModel = object;
+            scene.add( object );
+    
+        },
+        // called when loading is in progresses
+        function ( xhr ) {
+    
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    
+        },
+        // called when loading has errors
+        function ( error ) {
+    
+            console.log( 'An error happened' );
+    
+        }
+    );
+});
+
+
+document.getElementById("clear").addEventListener("click", () => {
+    if (loadedModel) {
+        scene.remove(loadedModel);
+    }
 });
 
 // Настройка фонового цвета
-document.getElementById("background-color").addEventListener("input", (event) => {
-    renderer.setClearColor(event.target.value);
-});
+renderer.setClearColor(0xffffff);
