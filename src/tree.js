03acb6babc31f825ed3ec10a06cc3ddbd311e28e
Prykhodchenko Oleksandra <119930416+Lesbica@users.noreply.github.com>
2023-11-06 20:08:29 +0200

    add texture and light

--- src/tree.js
+++ src/tree.js
@@ -8,6 +8,7 @@ const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('canv
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("canvas-container").appendChild(renderer.domElement);
+THREE.ColorManagement.enabled = true;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -17,6 +18,12 @@ let loadedModel = null;
 camera.position.set(0, 0, 5);
 controls.update();
 
+// Создаем источник света
+const light = new THREE.PointLight(0xffffff, 1); // Цвет и интенсивность света
+light.position.set(0, 5, 5); // Положение света
+
+scene.add(light);
+
 // Основной рендеринг
 const animate = function () {
     requestAnimationFrame(animate);
@@ -24,6 +31,51 @@ const animate = function () {
     renderer.render(scene, camera);
 };
 animate();
+
+// Функция для обработки движения света
+function moveLight(direction) {
+    const speed = 0.5;
+    if (direction === 'left') {
+        light.position.x -= speed;
+    } else if (direction === 'right') {
+        light.position.x += speed;
+    } else if (direction === 'up') {
+        light.position.y += speed;
+    } else if (direction === 'down') {
+        light.position.y -= speed;
+    }
+}
+
+// Функция для вращения света
+function rotateLight(direction) {
+    const rotationSpeed = 1;
+    if (direction === 'plus') {
+        light.rotation.x += rotationSpeed;
+        light.rotation.y += rotationSpeed;
+    } else if (direction === 'minus') {
+        light.rotation.x -= rotationSpeed;
+        light.rotation.y -= rotationSpeed;
+    }
+}
+
+// Обработчик события клавиатуры для перемещения света
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        moveLight('left');
+    } else if (event.key === 'ArrowRight') {
+        moveLight('right');
+    } else if (event.key === 'ArrowUp') {
+        moveLight('up');
+    } else if (event.key === 'ArrowDown') {
+        moveLight('down');
+    }else if (event.key === '+') {
+        rotateLight('plus');
+    } else if (event.key === '-') {
+        rotateLight('minus');
+    }
+});
+
+
 
 // Обработчики событий для добавления объектов
 document.getElementById("chair").addEventListener("click", () => {
@@ -114,8 +166,55 @@ document.getElementById("bed").addEventListener("click", () => {
 document.getElementById("clear").addEventListener("click", () => {
     if (loadedModel) {
         scene.remove(loadedModel);
+        camera.position.set(0, 0, 5);
+        controls.update();
     }
 });
 
 // Настройка фонового цвета
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(0x808080);
+
+const figureColorInput = document.getElementById("figure-color");
+
+figureColorInput.addEventListener("input", (event) => {
+    const colorHex = event.target.value; // Получаем выбранный цвет в формате HEX
+
+    // Преобразуем HEX в RGB и создаем объект цвета
+    const color = new THREE.Color(colorHex);
+
+    // Устанавливаем цвет для вашего объекта (например, loadedModel)
+    if (loadedModel) {
+        loadedModel.traverse((child) => {
+            if (child.isMesh) {
+                child.material.color = color;
+            }
+        });
+    }
+});
+
+
+
+const materialTypeSelect = document.getElementById("material-type");
+
+let material;
+let texture;
+materialTypeSelect.addEventListener("change", (event) => {
+    const selectedMaterialType = event.target.value;
+
+    if (selectedMaterialType === "iron") {
+        texture = new THREE.TextureLoader().load('texture/iron.jpg' );
+        material = new THREE.MeshPhongMaterial( {map: texture} );
+    } else if (selectedMaterialType === "wood") {
+        texture = new THREE.TextureLoader().load('texture/wood.jpg' ); 
+        material = new THREE.MeshToonMaterial( {map: texture} );
+    }
+
+    // Применяем выбранный материал к вашему объекту (например, loadedModel)
+    if (loadedModel) {
+        loadedModel.traverse((child) => {
+            if (child.isMesh) {
+                child.material = material;
+            }
+        });
+    }
+});
